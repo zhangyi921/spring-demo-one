e5ea788b875a4c7a584fb8f7baf007f26e8dd97a
Yi Zhang <yi16@ualberta.ca>
2019-06-25 12:33:30 -0600

    spring container

---
+++ src/com/springdemo/HelloSpringApp.java
@@ -0,0 +1,23 @@
+package com.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// retrive bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		// call method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// close the context
+		context.close();
+
+	}
+
+}
--- src/com/springdemo/MyApp.java
+++ src/com/springdemo/MyApp.java
@@ -4,7 +4,9 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Coach theCoach = new BaseballCoach();
+		
+		Coach theCoach = new TrackCoach();
+		
 		System.out.println(theCoach.getDailyWorkout());
 	}
 
