42401dda3d3b1c8e594590d150c78236062e664d
Yi Zhang <yi16@ualberta.ca>
2019-06-25 11:44:35 -0600

    learning spirng

---
+++ src/com/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.springdemo;
+
+public class BaseballCoach implements Coach{
+
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Spend 30 minutes on batting practice";
+	}
+}
---
+++ src/com/springdemo/MyApp.java
@@ -0,0 +1,11 @@
+package com.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Coach theCoach = new BaseballCoach();
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
