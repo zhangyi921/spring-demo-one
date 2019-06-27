2788c5d3c9ef2e6c97d181298de0b194d68f9a47
Yi Zhang <yi16@ualberta.ca>
2019-06-27 00:34:53 -0600

    bean scope and life cycles

--- src/com/springdemo/TrackCoach.java
+++ src/com/springdemo/TrackCoach.java
@@ -24,5 +24,15 @@ public class TrackCoach implements Coach {
 		// TODO Auto-generated method stub
 		return "Just do it: " + fortuneService.getFortune();
 	}
+	
+	// add an init method
+	public void doMyStartUpStuff() {
+		System.out.println("TrackCoach: inside method doMyStartUpStuff");
+	}
+	
+	// add an destory method
+		public void doMyCleanupStuff() {
+			System.out.println("TrackCoach: inside method doMyCleanupStuff");
+		}
 
 }
